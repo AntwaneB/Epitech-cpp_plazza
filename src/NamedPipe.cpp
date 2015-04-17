8973af280dcfa094fdeba3126a3bea15551d0469
Antoine Buchser <antoine.buchser@epitech.eu>
2015-04-17 15:18:02 +0200

    NamedPipe

--- src/NamedPipe.cpp
+++ src/NamedPipe.cpp
@@ -5,6 +5,9 @@
  * Created on April 13, 2015, 2:40 PM
  */
 
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <sstream>
 #include "NamedPipe.hpp"
 
@@ -20,6 +23,11 @@ NamedPipe::APipe::~APipe()
 NamedPipe::In::In(std::string const & path)
 	: APipe(path)
 {
+	if (access(_path.c_str(), R_OK) == -1)
+	{
+		if (mkfifo(_path.c_str(), 0644) == -1)
+			throw NamedPipe::Exception(std::string("NamedPipe::In::In: ") + strerror(errno));
+	}
 	_stream.open(_path);
 
 	if (_stream.fail())
@@ -29,6 +37,7 @@ NamedPipe::In::In(std::string const & path)
 NamedPipe::In::~In()
 {
 	_stream.close();
+	unlink(_path.c_str());
 }
 
 NamedPipe::In&	NamedPipe::In::operator>>(std::string & str)
@@ -44,6 +53,11 @@ NamedPipe::In&	NamedPipe::In::operator>>(std::string & str)
 NamedPipe::Out::Out(std::string const & path)
 	: APipe(path)
 {
+	if (access(_path.c_str(), W_OK) == -1)
+	{
+		if (mkfifo(_path.c_str(), 0644) == -1)
+			throw NamedPipe::Exception(std::string("NamedPipe::Out::Out: ") + strerror(errno));
+	}
 	_stream.open(_path);
 
 	if (_stream.fail())
@@ -53,6 +67,7 @@ NamedPipe::Out::Out(std::string const & path)
 NamedPipe::Out::~Out()
 {
 	_stream.close();
+	unlink(_path.c_str());
 }
 
 NamedPipe::Out&	NamedPipe::Out::operator<<(std::string const & str)
